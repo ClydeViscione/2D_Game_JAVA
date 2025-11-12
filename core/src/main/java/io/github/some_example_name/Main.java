5db43d3a39777594400aa3963d65abf39876c85b
ClydeViscione <191877265+ClydeViscione@users.noreply.github.com>
2025-11-12 11:07:27 +0100

    Blocks whitout collisions

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -3,18 +3,26 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.GL20;
+import java.util.ArrayList;
 
 public class Main extends ApplicationAdapter {
     ShapeRenderer shape;
     Ball ball;
     Paddle paddle;
-
+    ArrayList<Block> blocks = new ArrayList<>();
 
     @Override
     public void create () {
         shape = new ShapeRenderer();
         ball = new Ball(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2, 30 , 5, 5 );
         paddle = new Paddle(250, 50);
+        int blockWidth = 63;
+        int blockHeight = 20;
+        for (int y = Gdx.graphics.getHeight()/2; y < Gdx.graphics.getHeight(); y += blockHeight + 10) {
+            for (int x = 0; x < Gdx.graphics.getWidth(); x += blockWidth + 10) {
+                blocks.add(new Block(x, y, blockWidth, blockHeight));
+            }
+        }
     }
 
     @Override
@@ -25,6 +33,7 @@ public class Main extends ApplicationAdapter {
         shape.begin(ShapeRenderer.ShapeType.Filled);
         ball.draw(shape);
         paddle.draw(shape);
+        blocks.forEach(b -> b.draw(shape));
         shape.end();
     }
 }
